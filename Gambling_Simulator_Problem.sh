ad2b0704f14c4fbfde377dea534a15314d2210ca
anubhav230 <anubhavsingh230@gmail.com>
2020-06-08 19:43:34 +0530

    simulation for 20 days

--- Gambling_Simulator_Problem.sh
+++ Gambling_Simulator_Problem.sh
@@ -4,20 +4,27 @@ STAKE=100
 BET=1
 MAX_PROFIT_PER_DAY=50
 MAX_LOSS_PER_DAY=-50
-wallet=0
-while [[ $wallet -lt $MAX_PROFIT_PER_DAY && $wallet -gt $MAX_LOSS_PER_DAY ]]
+count=0
+totalAmount=0
+for (( day=1; day<=20; day++))
 do
-	betCheck=$((RANDOM%2))
-	if [ $WIN -eq $betCheck ]
-	then
-		wallet=$(($wallet+$BET))
-	else
-		wallet=$(($wallet-$BET))
-	fi
+	wallet=0
+	while [[ $wallet -lt $MAX_PROFIT_PER_DAY && $wallet -gt $MAX_LOSS_PER_DAY ]]
+	do
+		betCheck=$((RANDOM%2))
+		if [ $WIN -eq $betCheck ]
+		then
+			wallet=$(($wallet+$BET))
+		else
+			wallet=$(($wallet-$BET))
+		fi
+	done
+echo $wallet
+dayResult[((count++))]=$wallet
+totalAmount=$(($totalAmount+($STAKE+$wallet)))
 done
-
-echo "$wallet"
-echo "wallet amout =$(($STAKE+$wallet))"
+echo "result for every day ${dayResult[@]}"
+echo "Total amount by end of day $totalAmount"
 
 
 
