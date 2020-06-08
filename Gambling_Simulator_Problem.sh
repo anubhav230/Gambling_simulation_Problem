6ce61d0ca15f3966a494bf89021c59e82bff1099
anubhav230 <anubhavsingh230@gmail.com>
2020-06-08 16:54:38 +0530

    gambling for 1 bet

--- Gambling_Simulator_Problem.sh
+++ Gambling_Simulator_Problem.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -x
-stake=100
-bet=1
+WIN=1
+STAKE=100
+BET=1
+betCheck=$((RANDOM%2))
 
-echo "Every day stake= $stake $"
-echo "Per bet amount= $bet $"
+if [ $WIN -eq $betCheck ]
+then
+	echo "Winner won 1$"
+else
+	echo "Lost 1$ "
+fi
+
